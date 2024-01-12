a66f02e21144243e6e64636237a426ab040b06c6
ALXS <alxs.mnch@gmail.com>
2024-01-12 21:58:06 +0100

    some fix for the cdn

--- CheatSheet/cdnimport.js
+++ CheatSheet/cdnimport.js
@@ -9,7 +9,7 @@ document.head.appendChild(cssLink);
 // create link element for url CSS file
 var cssLink = document.createElement("link");
 cssLink.rel = "stylesheet";
-cssLink.href = "https://cdn.jsdelivr.net/gh/ALXS-GitHub/Markdown-Themes@latest/Aesthetic/cdnurl.min.css";
+cssLink.href = "https://cdn.jsdelivr.net/gh/ALXS-GitHub/Markdown-Themes@latest/CheatSheet/cdnurl.min.css";
 document.head.appendChild(cssLink);
 
 // create script element for MathJax
@@ -25,31 +25,48 @@ document.head.appendChild(mathJaxScript);
 
 // create script element to setup MathJax configuration
 var tocScript = document.createElement("script");
-tocScript.src = "https://cdn.jsdelivr.net/gh/ALXS-GitHub/Markdown-Themes@latest/Aesthetic/javascript/setupMathJax.min.js";
+tocScript.src = "https://cdn.jsdelivr.net/gh/ALXS-GitHub/Markdown-Themes@latest/CheatSheet/javascript/setupMathJax.min.js";
 tocScript.defer = true;
 document.head.appendChild(tocScript);
 
+// create script for the highlight.js library
+var highlightCssLink = document.createElement("link");
+highlightCssLink.rel = "stylesheet";
+highlightCssLink.id = "highlightCssLink";
+highlightCssLink.href = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/tokyo-night-dark.min.css";
+document.head.appendChild(highlightCssLink);
+
+var highlightScript = document.createElement("script");
+highlightScript.src = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js";
+highlightScript.defer = true;
+document.head.appendChild(highlightScript);
+
+highlightScript = document.createElement("script");
+highlightScript.src = "https://cdn.jsdelivr.net/gh/ALXS-GitHub/Markdown-Themes@latest/CheatSheet/javascript/setupHighlight";
+highlightScript.defer = true;
+document.head.appendChild(highlightScript);
+
 // create script element for TOC
 var tocScript = document.createElement("script");
-tocScript.src = "https://cdn.jsdelivr.net/gh/ALXS-GitHub/Markdown-Themes@latest/Aesthetic/javascript/toc.min.js";
+tocScript.src = "https://cdn.jsdelivr.net/gh/ALXS-GitHub/Markdown-Themes@latest/CheatSheet/javascript/toc.min.js";
 tocScript.defer = true;
 document.head.appendChild(tocScript);
 
 // create script element for the date
 var tocScript = document.createElement("script");
-tocScript.src = "https://cdn.jsdelivr.net/gh/ALXS-GitHub/Markdown-Themes@latest/Aesthetic/javascript/date.min.js";
+tocScript.src = "https://cdn.jsdelivr.net/gh/ALXS-GitHub/Markdown-Themes@latest/CheatSheet/javascript/date.min.js";
 tocScript.defer = true;
 document.head.appendChild(tocScript);
 
 // create script element for footnotes
 var tocScript = document.createElement("script");
-tocScript.src = "https://cdn.jsdelivr.net/gh/ALXS-GitHub/Markdown-Themes@latest/Aesthetic/javascript/footnotes.min.js";
+tocScript.src = "https://cdn.jsdelivr.net/gh/ALXS-GitHub/Markdown-Themes@latest/CheatSheet/javascript/footnotes.min.js";
 tocScript.defer = true;
 document.head.appendChild(tocScript);
 
 // create script element for custom blocks
 var tocScript = document.createElement("script");
-tocScript.src = "https://cdn.jsdelivr.net/gh/ALXS-GitHub/Markdown-Themes@latest/Aesthetic/javascript/customblocks.min.js";
+tocScript.src = "https://cdn.jsdelivr.net/gh/ALXS-GitHub/Markdown-Themes@latest/CheatSheet/javascript/customblocks.min.js";
 tocScript.defer = true;
 document.head.appendChild(tocScript);
 
